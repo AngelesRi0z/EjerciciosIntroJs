4a20d0e91148e1c7b6a765f18d494ab83148e7b6
Angeles Ri0z <rios.zendejas@gmail.com>
2022-01-10 12:56:52 -0600

    actualizacion

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
+// RECTANGULO EN ALERTA
 function areaRectangulo(){
-    
     var base = 5;
     var altura = 7;
-    
     var area = base * altura
-    
     alert("El area del rectangulo es igual a: "+ area);
-    
-    
     }
-    
     areaRectangulo();
-    
+// CIRCULO  EN CONSOLA
     function areaCirculo(radio){
         var pi = 3.1416;
         var resultado = pi * (radio**2);
         console.log("El area del circulo es igual a: " + resultado);
     }
-    
     areaCirculo(5);
-    
+//TRIANGULO  EN ALERTA
     function areaTriangulo(base,altura){
         var area = (base * altura)/2;
-        alert("El area del triangulo: " + area );
-        
+        alert("El area del triangulo: " + area );  
     }
-    
     areaTriangulo(10,5);
     
